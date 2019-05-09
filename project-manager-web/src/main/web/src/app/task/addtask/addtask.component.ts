588eb4d18e91f60138d10db70b13dd49e9947ca5
Alagappan Annamalai <alagappan.info@gmail.com>
2019-05-09 19:19:18 +0530

    IIHT Final Assessment - Project Manager Application - Add Task Page Initial Commit with Task & ParentTask Service Implementation

--- project-manager-web/src/main/web/src/app/task/addtask/addtask.component.ts
+++ project-manager-web/src/main/web/src/app/task/addtask/addtask.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../model/user";
+import {Project} from "../../model/project";
+import {TaskData} from "@angular/core/src/testability/testability";
+import {Task} from "../../model/task";
+import {ParentTask} from "../../model/parenttask";
 
 @Component({
   selector: 'app-addtask',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddtaskComponent implements OnInit {
 
+  users : User[];
+  projects : Project[];
+  taskModel : Task;
+  parentTask : ParentTask;
+  isParentTask : boolean = false;
+  selectedProjectId : number;
+  selectedParentTaskId : number;
+  selectedUserId : number;
+
+
+
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
